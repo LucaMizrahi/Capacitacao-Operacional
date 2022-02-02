f6bdba618cf0dd096262e4ffc035620d4c913687
joaobueno16 <jonnyleaobueno@hotmail.com>
2022-02-02 17:44:05 -0500

    terceira

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "76f5fa0deb1e4402"; }
+/******/ 		__webpack_require__.h = function() { return "43342cbac4cdf50b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome para <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1> 
 
         <p className={styles.description}>
           Get started by editing{' '}
